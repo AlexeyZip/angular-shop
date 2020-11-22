b5ee75e6d878fcdf275c7d162379fc1ffd1321d0
AlexeyZip <alexzipzap@gmail.com>
2020-11-22 14:05:58 +0200

    added firebase

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,7 +1,10 @@
+import { FbAuthResponse } from './../../../shared/interfaces';
+import { environment } from './../../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/shared/interfaces';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthService {
@@ -11,7 +14,12 @@ export class AuthService {
     return '';
   }
   login(user: User): Observable<any> {
-    return this.http.post('', user);
+    return this.http
+      .post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`,
+        user
+      )
+      .pipe(tap(this.setToken));
   }
 
   logout() {}
@@ -20,5 +28,7 @@ export class AuthService {
     return !!this.token;
   }
 
-  private setToken() {}
+  private setToken(response: FbAuthResponse) {
+    console.log(response);
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import { Environment } from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyCfuZhytm37_BiXIq9dw7wZ2Ayaqq5uhNI',
 };
