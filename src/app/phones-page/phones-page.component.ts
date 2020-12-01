68f0726f4df485f727cfbd9b35a7bced0f79123d
AlexeyZip <alexzipzap@gmail.com>
2020-12-01 14:40:02 +0200

    Outputting products

--- src/app/phones-page/phones-page.component.ts
+++ src/app/phones-page/phones-page.component.ts
@@ -1,4 +1,7 @@
+import { ProductService } from './../shared/peoduct.service';
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../shared/interfaces';
 
 @Component({
   selector: 'app-phones-page',
@@ -7,9 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhonesPageComponent implements OnInit {
 
-  constructor() { }
+  products$: Observable<Product[]>
 
-  ngOnInit(): void {
+  constructor(private productService: ProductService) { }
+
+  ngOnInit() {
+    this.products$ = this.productService.getAll()
   }
 
 }
--- src/app/shared/components/phone-page/phone-page.component.ts
+++ src/app/shared/components/phone-page/phone-page.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { Product } from '../../interfaces';
 
 @Component({
   selector: 'app-phone-page',
   templateUrl: './phone-page.component.html',
-  styleUrls: ['./phone-page.component.scss']
+  styleUrls: ['./phone-page.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class PhonePageComponent implements OnInit {
+
+@Input() product: Product
 
   constructor() { }
 
